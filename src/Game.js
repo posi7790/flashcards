f807e5c0c8478b0b6ae2cc463c2f53dc8aff7c12
Pol Sieira <polsieira@Pols-MacBook-Pro.local>
2019-08-13 08:46:21 -0600

    Complete Game class and pass all test

--- src/Game.js
+++ src/Game.js
@@ -2,16 +2,35 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/round');
+
 class Game {
   constructor() {}
 
+  start() {
+    const cards = prototypeQuestions.map(cardData => {
+      const card = new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer);
+      return card;
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+
+
+    this.currentRound = round;
+
+    this.printMessage(deck, round);
+    this.printQuestion(this.currentRound)
+  }
+
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,11 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 const Game = require('../src/Game');
 
-let game;
+let card1, card2, card3, deck, round, game;
 
 beforeEach(() => {
+  card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+  deck = new Deck([card1, card2, card3]);
+
+  round = new Round(deck);
+
   game = new Game();
 })
 

e25fc87d31fc1c23cc6a8366862d2914e9af4490
Pol Sieira <polsieira@Pols-MacBook-Pro.local>
2019-08-13 07:19:10 -0600

    Set up test for Deck class

---
+++ test/Deck-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be a instance of the class Deck', () => {
+
+  })
+
+});

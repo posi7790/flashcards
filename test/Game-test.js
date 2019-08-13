ca7abf28d1d3a3b71c67db49a7cefdec78753c7c
Pol Sieira <polsieira@Pols-MacBook-Pro.local>
2019-08-13 08:33:40 -0600

    Complete game test

---
+++ test/Game-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+
+let game;
+
+beforeEach(() => {
+  game = new Game();
+})
+
+describe('Game', function () {
+
+  it('should be a function', function () {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function () {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should keep track of current round', function () {
+    expect(game.round).to.deep.equal(round)
+  });
+});

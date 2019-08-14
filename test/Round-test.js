800cb2e3a9d9ac1b50d0e24ddc13a1d0522498d0
Pol Sieira <polsieira@Pols-MacBook-Pro.local>
2019-08-14 11:50:49 -0600

    Clean up global variables

--- test/Round-test.js
+++ test/Round-test.js
@@ -5,14 +5,14 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
-var card1, card2, card3, deck, round;
+var card1, card2, card3, round;
 
 beforeEach(() => {
   card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
   card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
-  deck = new Deck([card1, card2, card3]);
+  const deck = new Deck([card1, card2, card3]);
 
   round = new Round(deck);
 })
